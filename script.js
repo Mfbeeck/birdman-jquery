ef4600130b0ce578d5a7487de1f691d59cec14d7
Mfbeeck <mfbeeck@gmail.com>
2015-07-09 11:05:27 -0400

    finished birdman exercise

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var main = function() {
+	$('.dropdown img').click(function(){
+
+	$('.dropdown-menu').toggle();
+	});
+	// Another way to write the above:
+	$('.dropdown img').on('click',function(){
+
+	$('.dropdown-menu').toggle();
+	});
   
 }
  
